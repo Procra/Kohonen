4d4fdf73a361f9ef1ba0c9f357dcca231d999933
Stephen <stephen.gicquel@live.fr>
2016-08-30 09:35:45 +0200

    Add files via upload

---
+++ Kohonen/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Kohonen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new System.Windows.Forms.Panel();
+            this.generer = new System.Windows.Forms.Button();
+            this.Démarrer = new System.Windows.Forms.Button();
+            this.bgw1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.map.Location = new System.Drawing.Point(12, 12);
+            this.map.Name = "map";
+            this.map.Size = new System.Drawing.Size(202, 202);
+            this.map.TabIndex = 0;
+            this.map.Paint += new System.Windows.Forms.PaintEventHandler(this.map_Paint);
+            // 
+            // generer
+            // 
+            this.generer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.generer.Location = new System.Drawing.Point(13, 239);
+            this.generer.Name = "generer";
+            this.generer.Size = new System.Drawing.Size(75, 23);
+            this.generer.TabIndex = 1;
+            this.generer.Text = "Générer";
+            this.generer.UseVisualStyleBackColor = true;
+            this.generer.Click += new System.EventHandler(this.generer_Click);
+            // 
+            // Démarrer
+            // 
+            this.Démarrer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Démarrer.Location = new System.Drawing.Point(138, 239);
+            this.Démarrer.Name = "Démarrer";
+            this.Démarrer.Size = new System.Drawing.Size(75, 23);
+            this.Démarrer.TabIndex = 2;
+            this.Démarrer.Text = "Démarrer";
+            this.Démarrer.UseVisualStyleBackColor = true;
+            this.Démarrer.Click += new System.EventHandler(this.Démarrer_Click);
+            // 
+            // bgw1
+            // 
+            this.bgw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgw1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(225, 277);
+            this.Controls.Add(this.Démarrer);
+            this.Controls.Add(this.generer);
+            this.Controls.Add(this.map);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel map;
+        private System.Windows.Forms.Button generer;
+        private System.Windows.Forms.Button Démarrer;
+        private System.ComponentModel.BackgroundWorker bgw1;
+    }
+}
+
---
+++ Kohonen/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Threading;
+using AForge;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+
+namespace Kohonen
+{
+    public partial class Form1 : Form
+    {
+        private DistanceNetwork network;
+        private Bitmap mapBitmap;
+        private Random rand = new Random();
+        private int reptotal = 5000;
+        private double learningRate = 0.1;
+        private double radius = 15;
+        private bool stop = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Pour réduire le scintillement de l'image
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.UpdateStyles();
+
+            // Création du réseau de neurones
+            network = new DistanceNetwork(3, 100 * 100);
+
+            // Création de la bitmap (image composée de pixels)
+            mapBitmap = new Bitmap(200, 200, PixelFormat.Format24bppRgb);
+
+            // Génération aléatoire de pixels
+            RandomizeNetwork();
+        }
+
+        // Génération aléatoire des poids du réseau
+        private void RandomizeNetwork()
+        {
+            Neuron.RandRange = new DoubleRange(0, 255);
+
+            // Génération aléatoire du réseau
+            network.Randomize();
+
+            // Rafraichissement de la bitmap
+            Refresh();
+        }
+
+        // Rafrachit la map à partir des poids du réseau
+        private void Refresh()
+        {
+            // On verrouille l'image
+            BitmapData mapData = mapBitmap.LockBits(new Rectangle(0, 0, 200, 200),ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int stride = mapData.Stride;
+            int offset = stride - 200 * 3;
+            Layer layer = network[0];
+
+            unsafe
+            {
+                byte* ptr = (byte*)mapData.Scan0;
+
+                // pour chaque ligne
+                for (int y = 0, i = 0; y < 100; y++)
+                {
+                    // pour chaque pixel
+                    for (int x = 0; x < 100; x++, i++, ptr += 6)
+                    {
+                        Neuron neuron = layer[i];
+                        // rouge
+                        ptr[2] = ptr[2 + 3] = ptr[2 + stride] = ptr[2 + 3 + stride] = (byte)Math.Max(0, Math.Min(255, neuron[0]));
+                        // vert
+                        ptr[1] = ptr[1 + 3] = ptr[1 + stride] = ptr[1 + 3 + stride] = (byte)Math.Max(0, Math.Min(255, neuron[1]));
+                        // bleu
+                        ptr[0] = ptr[0 + 3] = ptr[0 + stride] = ptr[0 + 3 + stride] = (byte)Math.Max(0, Math.Min(255, neuron[2]));
+                    }
+                    ptr += offset;
+                    ptr += stride;
+                }
+            }
+
+            // on déverrouille l'image
+            mapBitmap.UnlockBits(mapData);
+
+            // Rafraichissement du contrôle
+            map.Invalidate();
+        }
+
+        // Lorsque la bitmap est redessinée
+        private void map_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics legraphe = e.Graphics;
+            // on redessine
+            legraphe.DrawImage(mapBitmap, 0, 0, 200, 200);
+        }
+
+        private void Démarrer_Click(object sender, EventArgs e)
+        {
+            reptotal = 50;
+            learningRate = 0.1;
+            radius = 15;
+            // démarrage du thread
+            stop = false;
+            bgw1.RunWorkerAsync();
+        }
+
+        private void generer_Click(object sender, EventArgs e)
+        {
+            RandomizeNetwork();
+        }
+
+        private void bgw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Algorythme d'apprentissage pour les cartes auto-organisatrices (SOM)
+            SOMLearning apprentissage = new SOMLearning(network);
+
+            // entrée
+            double[] entree = new double[3];
+
+            double fixedLearningRate = learningRate / 10;
+            double driftingLearningRate = fixedLearningRate * 9;
+
+            // Nombre de répétitions
+            int répètes = 0;
+
+            while (!stop){
+                apprentissage.LearningRate = driftingLearningRate * (reptotal - répètes) / reptotal + fixedLearningRate;
+                apprentissage.LearningRadius = (double)radius * (reptotal - répètes) / reptotal;
+
+                entree[0] = rand.Next(256);
+                entree[1] = rand.Next(256);
+                entree[2] = rand.Next(256);
+
+                apprentissage.Run(entree);
+
+                // Rafraichissement de la bitmap toutes les 50 répétitions
+                if ((répètes % 10) == 9){
+                    Refresh();
+                }
+                répètes++;
+                if (répètes >= reptotal)
+                    break;
+            }
+        }
+    }
+}
